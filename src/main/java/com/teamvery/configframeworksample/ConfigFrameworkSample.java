0039000ca84452bc598f3e1cbb20ddf82772ac70
Kill00 <hellow1211other@gmail.com>
2021-01-19 23:31:37 +0900

    Add reload command

--- src/main/java/com/teamvery/configframeworksample/ConfigFrameworkSample.java
+++ src/main/java/com/teamvery/configframeworksample/ConfigFrameworkSample.java
@@ -4,21 +4,25 @@ import com.teamvery.configframework.cfg;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class ConfigFrameworkSample extends JavaPlugin {
 
-    String plugin_name = this.getName();
-    String config = "config.yml";
-    String work = "work.yml";
+    public static String plugin_name = "Config-Framework-Sample";
+    public static String conf = "config.yml";
+    public static String work = "work.yml";
 
     @Override
     public void onEnable() {
+        Objects.requireNonNull(getCommand("reload")).setExecutor(new Reload());
+
         getConfig().options().copyDefaults(true);
         saveDefaultConfig();
 
-        cfg.makeData(plugin_name, config);
+        cfg.makeData(plugin_name, conf);
         cfg.makeData(plugin_name, work);
 
-        if (cfg.get(plugin_name, config).getBoolean("활성화")) {
+        if (cfg.get(plugin_name, conf).getBoolean("활성화")) {
             getLogger().info(ChatColor.GREEN + "Config Framework Sample Plugin WORK!");
         } else {
             getLogger().info(ChatColor.RED + "Config Framework Sample Plugin NOT!!! WORK!");
---
+++ src/main/java/com/teamvery/configframeworksample/Reload.java
@@ -0,0 +1,38 @@
+package com.teamvery.configframeworksample;
+
+import com.teamvery.configframework.cfg;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import static com.teamvery.configframeworksample.ConfigFrameworkSample.*;
+
+public class Reload implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
+
+        cfg.reload(plugin_name, conf);
+        if (!(sender instanceof Player)) {
+            System.out.println("§a리로드가 완료되었습니다!");
+        } else {
+            Player player = (Player) sender;
+            player.sendMessage("§a리로드가 완료되었습니다!");
+        }
+
+        if (cfg.get(plugin_name, ConfigFrameworkSample.conf).getBoolean("활성화")) {
+            System.out.println(ChatColor.GREEN + "Config Framework Sample Plugin WORK!");
+        } else {
+            System.out.println(ChatColor.RED + "Config Framework Sample Plugin NOT!!! WORK!");
+        }
+        if (!cfg.get(plugin_name, work).getBoolean("니")) {
+            System.out.println(ChatColor.RED + "Nice NOT!! Work!");
+        } else {
+            System.out.println(ChatColor.GREEN + "NiceWork!");
+        }
+        return false;
+    }
+}

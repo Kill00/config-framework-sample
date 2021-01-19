39fdfaf956c1768c475ba6e794ca104a49577d52
Kill00 <hellow1211other@gmail.com>
2021-01-19 23:09:38 +0900

    Add Var(String)

--- src/main/java/com/teamvery/configframeworksample/ConfigFrameworkSample.java
+++ src/main/java/com/teamvery/configframeworksample/ConfigFrameworkSample.java
@@ -6,23 +6,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ConfigFrameworkSample extends JavaPlugin {
 
+    String plugin_name = this.getName();
+    String config = "config.yml";
+    String work = "work.yml";
+
     @Override
     public void onEnable() {
         getConfig().options().copyDefaults(true);
         saveDefaultConfig();
 
-        cfg.makeData("Config-Framework-Sample", "config.yml");
-        cfg.makeData("Config-Framework-Sample", "work.yml");
+        cfg.makeData(plugin_name, config);
+        cfg.makeData(plugin_name, work);
 
-        if (cfg.get("Config-Framework-Sample", "config.yml").getBoolean("활성화")) {
+        if (cfg.get(plugin_name, config).getBoolean("활성화")) {
             getLogger().info(ChatColor.GREEN + "Config Framework Sample Plugin WORK!");
         } else {
             getLogger().info(ChatColor.RED + "Config Framework Sample Plugin NOT!!! WORK!");
         }
-        if (!cfg.get("Config-Framework-Sample", "work.yml").getBoolean("니")) {
+        if (!cfg.get(plugin_name, work).getBoolean("니")) {
             getLogger().info(ChatColor.RED + "Nice NOT!! Work!");
         } else {
-            getLogger().info(ChatColor.GREEN + "Nice NOT!! Work!");
+            getLogger().info(ChatColor.GREEN + "NiceWork!");
         }
     }
 }

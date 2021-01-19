a21ac76b2c8e79d18552e413eb52dac288f37e37
Kill00 <hellow1211other@gmail.com>
2021-01-19 22:32:37 +0900

    Add Color & Remove Depend in plugin.yml

--- src/main/java/com/teamvery/configframeworksample/ConfigFrameworkSample.java
+++ src/main/java/com/teamvery/configframeworksample/ConfigFrameworkSample.java
@@ -1,6 +1,7 @@
 package com.teamvery.configframeworksample;
 
 import com.teamvery.configframework.cfg;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ConfigFrameworkSample extends JavaPlugin {
@@ -14,10 +15,14 @@ public final class ConfigFrameworkSample extends JavaPlugin {
         cfg.makeData("Config-Framework-Sample", "work.yml");
 
         if (cfg.get("Config-Framework-Sample", "config.yml").getBoolean("활성화")) {
-            getLogger().info("Config Framework Sample Plugin WORK!");
+            getLogger().info(ChatColor.GREEN + "Config Framework Sample Plugin WORK!");
+        } else {
+            getLogger().info(ChatColor.RED + "Config Framework Sample Plugin NOT!!! WORK!");
         }
         if (!cfg.get("Config-Framework-Sample", "work.yml").getBoolean("니")) {
-            getLogger().info("NiceWork!");
+            getLogger().info(ChatColor.RED + "Nice NOT!! Work!");
+        } else {
+            getLogger().info(ChatColor.GREEN + "Nice NOT!! Work!");
         }
     }
 }
